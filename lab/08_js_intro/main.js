c536cd126e4dea427a47f520c79d17fbc2f50834
Lily Clempson <lily-london@hotmail.com>
2017-03-16 10:35:13 -0400

    adding responsive work folder

---
+++ lab/08_js_intro/main.js
@@ -0,0 +1,48 @@
+console.log('hello world');
+
+
+var myStudentDebt = 800;
+myStudentDebt
+
+var myLuckyNumbers = [ 4, 7, 8, 16];
+
+console.log(myLuckyNumbers);
+console.log(myLuckyNumbers[0]);
+
+myLuckyNumbers[2] = 36;
+console.log(myLuckyNumbers[2]);
+
+add(10,10);
+
+function addFive(x) {
+	return x + 5;
+}
+
+function add(x, y) {
+	console.log(x + y);
+}
+
+addFive(10);
+add(8,9);
+add("Bryant" + "Wells");
+
+
+var myAge = 20;
+
+if (myAge < 21) {
+	console.log("u cant party");
+} else {
+	console.log("party on garth");
+}
+
+
+console.log(addFive(6));
+
+var hamburgers = 0;
+
+for (var i = 0; i < 10; i = i + 1) {
+	hamburgers++;
+	console.log(hamburgers);
+}
+
+
