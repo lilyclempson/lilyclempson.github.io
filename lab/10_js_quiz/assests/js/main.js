9b4b0326704416812b6966af0bc6c16aa7dd8f72
Lily clempson <lilyclempson@Lilys-MBP.wireless.newschool.edu>
2017-04-06 09:35:27 -0400

    adding javascript quiz

---
+++ lab/10_js_quiz/assests/js/main.js
@@ -0,0 +1,55 @@
+var html = document.documentElement;
+
+var myCheckerboard = 0;
+
+var myCheckerboard (w, h) {
+	return 5 * 5;
+}
+  var myButton = document.getElementById("my-button");
+
+  myCheckerboard(5,5);
+    myButton.addEventListener("click", function() {
+
+   console.log("my checkerboard = 5 * 5" + myCheckerboard);
+}); 
+
+//------------------------------------------------------
+
+
+html.addEventListener("click", function(event) {
+   console.log(triangle + " , " + height)
+
+
+function myFunction (h) {
+	return 3 * 2;
+}
+  myFunction(3,2);
+ 
+console.log(3 + 2)
+
+
+//-----------------------------------------------------
+
+var myCheckerboard = 0;
+
+var myButton = document.getElementById("my-button");
+
+myButton.addEventListener("click", function() {
+     myCheckerboard += 100;
+     
+     console.log("5 * 5" + myCheckerboard);
+}); 
+
+
+var myPyramid = 0;
+
+var myButton = document.getElementById("my-button");
+
+myButton.addEventListener("click", function() {
+     myPyramid += 100;
+     
+     console.log("5 * " + myPyramid);
+}); 
+
+
+
